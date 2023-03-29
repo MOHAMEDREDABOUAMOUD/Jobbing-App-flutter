04d8cebd52516afc719210ea2b7b7a3b7cc9f111
MOHAMEDREDABOUAMOUD <mohamedredabouamoud@gmail.com>
2023-03-29 01:09:30 +0000

    add chat with contacts in database

---
+++ signin_signup/lib/pages/chat_page.dart
@@ -0,0 +1,258 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:signin_signup/pages/home_page.dart';
+
+final _firestore = FirebaseFirestore.instance;
+late User SignedInUser; //this will give me email
+
+class ChatScreen extends StatefulWidget {
+  static const String screenRoute = 'chat_screen';
+
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
+
+  final _auth = FirebaseAuth.instance;
+  String? MessageText; //this will give me message
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        SignedInUser = user;
+        print(SignedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // void getMessages() async {
+  //   final messages = await _firestore.collection('messages').get();
+  //   for (var msg in messages.docs){
+  //     print(msg.data());
+  //   }
+  // }
+
+  // void messagesStream() async {
+  //   await for (var spanshot in _firestore.collection('messages').snapshots()){
+  //     for (var msg in spanshot.docs){
+  //       print(msg.data());
+  //     }
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 93, 82, 84),
+        title: Row(
+          //mainAxisSize: MainAxisSize.min,
+          children: [
+            // Image.asset(
+            //   'assets/images/logo.png',
+            //   height: 35,
+            // ),
+            // SizedBox(
+            //   width: 10,
+            // ),
+            Text(
+              "User: ${HomePage.receiveruser}",
+              style: TextStyle(fontSize: 10),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _auth.signOut();
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.close),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          //mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            MessageStreamBuilder(),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: Color.fromARGB(255, 190, 131, 169),
+                    width: 2,
+                  ),
+                ),
+              ),
+              child: Row(
+                //mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: messageTextController,
+                      onChanged: (value) {
+                        MessageText = value;
+                      },
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 20,
+                        ),
+                        hintText: 'Write your Message here ...',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        messageTextController.clear();
+                        // Get a reference to the Firestore document you want to update
+                        final DocumentReference docRef = FirebaseFirestore
+                            .instance
+                            .collection('messages')
+                            .doc(HomePage.uiddoc);
+                        print(HomePage.uiddoc);
+
+                        // Update the fields you want to change
+                        docRef.update({
+                          'text': MessageText,
+                          'time': FieldValue.serverTimestamp(),
+                        });
+                      },
+                      child: Text(
+                        'Send',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 160, 98, 125),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessageStreamBuilder extends StatelessWidget {
+  const MessageStreamBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').orderBy('time').snapshots(),
+      builder: (context, snapshot) {
+        List<MessageLine> messageWidgets = [];
+
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.blue,
+            ),
+          );
+        }
+
+        final messages = snapshot.data!.docs.reversed;
+
+        for (var msg in messages) {
+          final messagereceiver = msg.get('receiver'); //receiver
+          final messageSender = msg.get('sender');
+          final messageText = msg.get('text');
+          final currentUser = SignedInUser.email;
+          final receiverUser = HomePage.receiveruser;
+
+          final messageWidget = MessageLine(
+              sender: messageSender,
+              isMe: currentUser == messageSender,
+              isRec: receiverUser == messagereceiver,
+              text: messageText);
+          messageWidgets.add(messageWidget);
+        }
+
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            children: messageWidgets,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MessageLine extends StatelessWidget {
+  const MessageLine(
+      {this.text,
+      this.sender,
+      required this.isMe,
+      required this.isRec,
+      super.key});
+
+  final String? sender;
+  final String? text;
+  final bool isMe;
+  final bool isRec;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        //mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$sender',
+            style: TextStyle(fontSize: 12, color: Colors.black45),
+          ),
+          Material(
+            elevation: 5,
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  )
+                : BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+            color: isMe ? Colors.blue[800] : Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Text('$text',
+                  style: TextStyle(
+                      fontSize: 15, color: isMe ? Colors.white : Colors.black)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- signin_signup/lib/pages/home_page.dart
+++ signin_signup/lib/pages/home_page.dart
@@ -1,10 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:signin_signup/pages/chat_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   //final FileImage? profile;
+  static String receiveruser = "";
+  static String uiddoc = "";
+  final _firestore = FirebaseFirestore.instance;
+  late User SignedInUser;
   final String name, phone, email, description, profile;
   HomePage(
       {super.key,
@@ -14,43 +22,160 @@ class HomePage extends StatelessWidget {
       required this.email,
       required this.description});
 
-  // sign user out methode
-  void signUserOut() {
-    FirebaseAuth.instance.signOut();
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        widget.SignedInUser = user;
+        print(widget.SignedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  String generateUserId() {
+    final int rand = Random()
+        .nextInt(1000000); // Generate a random integer between 0 and 999999
+    final String timestamp = DateTime.now()
+        .millisecondsSinceEpoch
+        .toString(); // Get the current timestamp in milliseconds
+    return '$timestamp$rand'; // Concatenate the timestamp and random integer to create a unique ID
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                signUserOut();
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.logout))
-        ],
-      ),
-      body: Center(
-        child: Column(
+        backgroundColor: Color.fromARGB(255, 93, 82, 84),
+        title: Row(
           children: [
-            Text("LOGGEDIN : "),
-            CircleAvatar(
-              radius: 40,
-              backgroundImage: profile != ""
-                  ? NetworkImage(profile) //profile as ImageProvider
-                  // ignore: unnecessary_cast
-                  : NetworkImage(
-                      'https://www.w3schools.com/howto/img_avatar.png'),
-            ),
-            Text("Name : $name"),
-            Text("E-mail : $email"),
-            Text("Phone : $phone"),
-            Text("Description : $description"),
+            // Image.asset(
+            //   'assets/images/logo.png',
+            //   height: 35,
+            // ),
+            // SizedBox(
+            //   width: 10,
+            // ),
+            Text('List Contacts'),
           ],
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              //_auth.signOut();
+              //Navigator.pop(context);
+            },
+            icon: Icon(Icons.close),
+          )
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: _db.collection('users').snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return const Center(child: Text('Error loading emails'));
+          }
+          final List<String> emails = snapshot.data?.docs
+                  .map((doc) => doc.data()['email'] as String)
+                  .toList() ??
+              [];
+          final List<Widget> buttons = emails
+              .map((email) => ElevatedButton(
+                    onPressed: () {
+                      final String userId = generateUserId();
+
+                      // Get a reference to the Firestore collection where you want to add the document
+                      final CollectionReference messagesRef =
+                          FirebaseFirestore.instance.collection('messages');
+
+                      // Create a reference to the document with the preferred UID
+                      final DocumentReference docRef = messagesRef.doc(userId);
+
+                      // Set the data for the document
+                      docRef.set({
+                        'sender': widget.SignedInUser.email,
+                        'receiver': email,
+                        'text': '',
+                        'time': FieldValue.serverTimestamp()
+                      });
+                      HomePage.uiddoc = userId;
+                      HomePage.receiveruser = email;
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (BuildContext context) => const ChatScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(email),
+                  ))
+              .toList();
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: buttons,
+            ),
+          );
+        },
       ),
     );
   }
+
+  // sign user out methode
+  // void signUserOut() {
+  //   FirebaseAuth.instance.signOut();
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       actions: [
+  //         IconButton(
+  //             onPressed: () {
+  //               signUserOut();
+  //               Navigator.pop(context);
+  //             },
+  //             icon: Icon(Icons.logout))
+  //       ],
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         children: [
+  //           Text("LOGGEDIN : "),
+  //           CircleAvatar(
+  //             radius: 40,
+  //             backgroundImage: widget.profile != ""
+  //                 ? NetworkImage(widget.profile) //profile as ImageProvider
+  //                 // ignore: unnecessary_cast
+  //                 : NetworkImage(
+  //                     'https://www.w3schools.com/howto/img_avatar.png'),
+  //           ),
+  //           Text("Name : ${widget.name}"),
+  //           Text("E-mail : ${widget.email}"),
+  //           Text("Phone : ${widget.phone}"),
+  //           Text("Description : ${widget.description}"),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
